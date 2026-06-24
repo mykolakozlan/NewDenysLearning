ed433432498cceb3fb3ef5f339e0a9b501a16d7e
NikolasK88 <maxsuccess4@gmail.com>
2026-06-24 08:07:15 +0300

    Second lesson

---
+++ lesson_2.py
@@ -0,0 +1,21 @@
+
+
+#Numbers
+
+# int()  1, 2, 3
+# flaot() 1.3, 2.7, 3.9
+# complex() (1 + 2j)
+# decimal() 1.3
+
+
+# String
+# str()
+
+# bool Бульовий тип даних
+# None - NoneType
+
+
+# list список
+# tuple() кортеж
+# dict() словники
+# set() множина
