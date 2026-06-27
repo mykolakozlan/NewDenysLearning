3794c18c622a64bab8295aa54cb2a62fef6f3a46
NikolasK88 <maxsuccess4@gmail.com>
2026-06-27 11:16:43 +0300

    Third lesson

---
+++ lesson_3.py
@@ -0,0 +1,79 @@
+#
+# # snake_case my_first_lesson # CamelCase  myFirstLesson
+#
+# box_1 = "apple1111"
+# my_first_variable = 1
+#
+# print(my_first_variable)
+#
+# my_print = 8
+#
+# print(my_print)
+#
+# # id()
+# # globals()
+# # type()
+#
+# item_id = 751
+# proj_globals = [1, 2, 3, 4]
+# product_type = "food"
+#
+#
+#
+# weidth = 100  # x = 100
+
+
+
+
+# num_1 = 5
+# num_2 = num_1
+#
+# print(id(num_1))
+#
+# print(num_2) # 5
+#
+# num_1 = 10
+# print(id(num_1))
+#
+# print(num_2) # 5
+
+
+# num_1 = 9
+# num_2 = 2
+#
+# result_add = num_1 + num_2
+# result_sub = num_1 - num_2
+# result_div = num_1 / num_2
+# result_div2 = num_1 // num_2 #цілочилене ділення
+# result_div3 = num_1 % num_2 #залишок від ділення
+#
+# result_mul = num_1 * num_2
+# result_power = num_1 ** 2 # ступінь
+# result_root = num_1 ** 0.5 # корінь
+#
+# print(result_root, type(result_root))
+
+# STRING
+
+# my_text = 'some text 1 $ _'
+# my_text = "some text"
+# my_text = '''some text'''
+# my_text = """some text"""
+
+#
+# greetings = 'Hi, Im Nick'
+# greetings = '''Hi, I'm Nick, my company name is "Google"'''
+
+# print(greetings, type(greetings))
+
+
+# name = "Nick"
+# age = 12
+#
+# # greetings = 'Hi, Im ' + name + ' my age is {' + str(age) + '}'
+# greetings = f'Hi, Im {name} my age is {age}'
+#
+# print(greetings)
+
+
+
